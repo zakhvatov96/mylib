0f55421e5d32f37e442e8e1896602471fe4b03e7
Dima <example@gmail.com>
2026-04-02 23:08:04 +0200

    dynamic modal is finished

--- dist/script.js
+++ dist/script.js
@@ -32,7 +32,7 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.dropdown = function () {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
 
-_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function (created) {
   for (let i = 0; i < this.length; i++) {
     const target = this[i].getAttribute('data-target');
     (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).click(e => {
@@ -40,22 +40,72 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
       (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeIn(500);
       document.body.style.overflow = 'hidden';
     });
-    const closeElements = document.querySelectorAll('[data-close]');
+    const closeElements = document.querySelectorAll(`${target} [data-close]`);
     closeElements.forEach(item => {
       (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(item).click(() => {
         (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeOut(500);
         document.body.style.overflow = '';
+        if (created) {
+          document.querySelector(target).remove();
+        }
       });
     });
-    (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').click(e => {
+    (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).click(e => {
       if (e.target.classList.contains('modal')) {
         (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeOut(500);
         document.body.style.overflow = '';
+        if (created) {
+          document.querySelector(target).remove();
+        }
       }
     });
   }
 };
 (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(['[data-toggle = modal]']).modal();
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.createModal = function ({
+  text,
+  btns
+} = {}) {
+  for (let i = 0; i < this.length; i++) {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+    const buttons = [];
+    for (let j = 0; j < btns.count; j++) {
+      let btn = document.createElement('button');
+      btn.classList.add('btn', ...btns.settings[j][1]);
+      btn.textContent = btns.settings[j][0];
+      if (btns.settings[j][2]) {
+        btn.setAttribute('data-close', 'true');
+      }
+      ;
+      if (btns.settings[j][3] && typeof btns.settings[j][3] == 'function') {
+        btn.addEventListener('click', btns.settings[j][3]);
+      }
+      buttons.push(btn);
+    }
+    modal.innerHTML = `
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<button class="close" data-close>&times;</button>
+					<div class="modal-header">
+						<div class="modal-title">${text.title}</div>
+					</div>
+					<div class="modal-body">
+						${text.body}
+					</div>
+					<div class="modal-footer">
+
+					</div>
+				</div>
+			</div>
+		`;
+    modal.querySelector('.modal-footer').append(...buttons);
+    document.body.appendChild(modal);
+    (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).modal(true);
+    (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i].getAttribute('data-target')).fadeIn(500);
+  }
+};
 
 /***/ },
 
@@ -515,6 +565,18 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _lib_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lib/lib */ "./src/js/lib/lib.js");
 
+$('#trigger').click(() => $('#trigger').createModal({
+  text: {
+    title: 'Modal',
+    body: 'orem ipsum dolor sit amet consectetur adipisicing elit. Animi, porro ab earum, itaque, cupiditate aspernatur voluptatum libero rem magni enim officiis tempora obcaecati. Molestias sapiente iure, ad dolorem rem minima?'
+  },
+  btns: {
+    count: 2,
+    settings: [['Close', ['btn-danger', 'mr-10'], true], ['Save changes', ['btn-success'], false, () => {
+      alert('Data saved');
+    }]]
+  }
+}));
 })();
 
 /******/ })()
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 import './lib/lib';
+
+$('#trigger').click(() => $('#trigger').createModal({
+	text: {
+		title: 'Modal',
+		body: 'orem ipsum dolor sit amet consectetur adipisicing elit. Animi, porro ab earum, itaque, cupiditate aspernatur voluptatum libero rem magni enim officiis tempora obcaecati. Molestias sapiente iure, ad dolorem rem minima?'
+	},
+	btns: {
+		count: 2,
+		settings: [
+			['Close',
+				['btn-danger', 'mr-10'],
+				true
+			],
+			[
+				'Save changes',
+				['btn-success'],
+				false,
+				() => {
+					alert('Data saved')
+				}
+			]
+		]
+	}
+}))
